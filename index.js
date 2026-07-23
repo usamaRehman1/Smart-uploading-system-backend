4d1ac3041575dba2e3dfbeb04dea7225b8b515c6
Muhammad Usama <46845901+usamaRehman1@users.noreply.github.com>
2026-07-23 19:41:57 +0500

    cors installation

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 import express from "express";
 import { connectDB } from "./src/config/connectionDB.js";
 import { ENV } from "./src/constant.js";
+import cors from "cors";
 import router from "./src/route.js";
 import "./src/worker/worker.js";
 
 connectDB();
 const app = express();
 app.use(express.json());
+app.use(cors({
+    origin: "*", // For development
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
 app.use("/api", router);
 
 app.get("/health", (req, res) => {
